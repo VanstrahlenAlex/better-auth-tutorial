792a26499a0a493614480b1c6843b8ffdc723b2b
Alexander Van strahlen <75139319+VanstrahlenAlex@users.noreply.github.com>
2025-12-23 14:58:21 -0500

    23/12/2025

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,9 +1,13 @@
-
 import { PrismaClient } from "@prisma/client";
+import { config } from "dotenv";
+
+// Cargar variables de entorno
+config();
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma =
-	globalForPrisma.prisma || new PrismaClient();
+	globalForPrisma.prisma ||
+	new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
